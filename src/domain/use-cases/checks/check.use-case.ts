71f607e774d793d4730e1a55b00721bc62a630fd
Dieguidev <diegogaraycullas@gmail.com>
2024-02-27 17:46:50 -0500

    check.use-case.ts: se creo las dependencia de success y error para inyectarlas en CheckService

--- src/domain/use-cases/checks/check.use-case.ts
+++ src/domain/use-cases/checks/check.use-case.ts
@@ -3,20 +3,32 @@ interface CheckServiceUseCase {
 }
 
 
+type SucessCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
 
 export class CheckService implements CheckServiceUseCase {
+
+  constructor(
+    private readonly successCallback: SucessCallback,
+    private readonly errorCallback: ErrorCallback,
+  ) {}
+
+
   public async execute(url: string): Promise<boolean> {
     try {
       const req = await fetch(url);
       if (!req.ok) {
         throw new Error(`Error on check service ${url}`);
       }
-      console.log(`${url} is ok`);
+
+      this.successCallback();
+
 
       return true;
     } catch (error) {
       console.log(`${error}`);
-
+      this.errorCallback(`${error}`);
       return false;
     }
 
