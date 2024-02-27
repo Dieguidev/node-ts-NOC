57a1d13ba96383d05c8b9a69f958ae27a42e2ec9
Dieguidev <diegogaraycullas@gmail.com>
2024-02-27 17:11:34 -0500

    check.esu-case.ts: se crea el caso de uso para el checkeo de una url y se prueba en server.ts con url de google

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { CheckService } from "../domain/use-cases/checks/check.use-case";
 import { CronService } from "./cron/cron-service";
 
 
@@ -11,8 +12,7 @@ export class Server {
     CronService.createJob(
       '*/5 * * * * *',
       () =>{
-        const date = new Date();
-        console.log('5 seconds', date);
+        new CheckService().execute('https://google.com');
 
       }
     )
