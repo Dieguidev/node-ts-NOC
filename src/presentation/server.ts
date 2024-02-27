2d980056ff2afe9b1cecb36a610eae2294115153
Dieguidev <diegogaraycullas@gmail.com>
2024-02-27 13:24:17 -0500

    instalado CRON y probado en server.ts

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,4 @@
-
+import { CronJob } from 'cron';
 
 
 
@@ -7,5 +7,15 @@ export class Server {
   public static start() {
     console.log('Server started...');
 
+
+    const job = new CronJob(
+      '*/2 * * * * *', // cronTime
+      function () {
+        const date = new Date();
+        console.log(date);
+      }, // onTick
+    );
+
+    job.start()
   }
 }
