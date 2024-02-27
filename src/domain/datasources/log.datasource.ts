c14074d52aaa67e2a258d5d99a210b5ee01cb2ce
Dieguidev <diegogaraycullas@gmail.com>
2024-02-27 18:40:14 -0500

    se crean log.datasource.ts para que sea las reglas de negocio y se crea el archivo log.repository.ts para acceder al repositorio, todo implementado siguiendo clean code

---
+++ src/domain/datasources/log.datasource.ts
@@ -0,0 +1,17 @@
+import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
+
+
+//* la clase abstracta evita que se pueda instanciar
+// por lo cual nos sirve para obligar lo que defina en esta clase
+
+
+// sobre otras clases que hereden de esta clase
+export abstract class LogDataSource {
+  abstract saveLog(log: LogEntity): Promise<void>;
+  abstract getLog(severityLevel: LogSeverityLevel): Promise<LogEntity[]>;
+
+
+}
+
+
+
